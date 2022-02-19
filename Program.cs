83c269a1af61a054acfbc55ad73f0675198a81ce
Rofeeah Atinuke <raadebayo@sidmach.com>
2022-02-19 04:50:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("1234567890");
+           // drawSymbol(10);
+           // minMax(new List<long>(){ 256741038,623958417,467905213,714532089,938071625});
+            MaxCount(new List<int> { 3, 2, 1, 3 });
+        }
+
+        static void drawSymbol(int n) {
+            StringBuilder symbol = 
+                new StringBuilder("#".Length * n).Insert(0, "#", n);
+         
+            string symbolXtra = symbol.ToString();
+            for (int i = 0, j = n - 1; i < n; i++, j--)
+            {
+                Console.WriteLine(
+                    symbol.Replace("#", " ", 0, j));
+                symbol.Clear();
+                symbol.Append(symbolXtra);
+              
+            }
+
+        }
+
+        static void minMax(List<long> n)
+        {
+            var nMax = n.Max();
+            var nMin = n.Min();
+
+            Console.WriteLine(n.Where(x => x != nMax).Sum() + " " + n.Where(x => x != nMin).Sum());
+        }
+
+        static void MaxCount(List<int> n)
+        {
+
+            Console.WriteLine(n.Where(x => x == n.Max()).Count());
+
+            Console.WriteLine((from x in n where x == n.Max() select x).Count());
+
+
+        }
+    }
+}
